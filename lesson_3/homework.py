19dc07ed943ad6a8741b85af6567e590c22f158c
meh84 <meh84741@gmail.com>
2023-08-16 19:22:05 +0300

    asaa

---
+++ lesson_3/homework.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).absolute().parent.parent
+
+file = open(ROOT_DIR / "rockyou.txt", "r", encoding="utf-8")
+
+text = file.readline()
+
+results = []
+
+while True:
+    try:
+        word = file.readline()
+        if word.startswith("user"):
+            my_input = input(
+                f"Додати слово '{word.strip()}' до результату? (y/n): "
+            ).lower()
+            if my_input == "y":
+                results.append(word)
+            else:
+                print("wrong answer")
+        if not word:
+            break
+    except Exception:
+        break
+
+file.close()
+
+print("Результати:", "".join(results))
