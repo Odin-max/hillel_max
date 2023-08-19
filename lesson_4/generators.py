124a60a36958e7596927bc43eac1f2fcf9bb5cc6
meh84 <meh84741@gmail.com>
2023-08-19 20:38:34 +0300

    lesson_4 added

---
+++ lesson_4/generators.py
@@ -0,0 +1,21 @@
+def foo():
+    print("Hello, i'm foo")
+
+def bar(function):
+    function.__call__()
+
+def baz():
+    yield 1
+    yield 2
+    yield 3
+    yield 4
+    yield 5
+
+gen = baz()
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+gen.send(int(input("Next number: ")))
+print(next(gen))
