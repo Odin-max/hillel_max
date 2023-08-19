60af83bdc7d6b4d96d8e80a187a5387c441f5170
meh84 <meh84741@gmail.com>
2023-08-19 20:38:34 +0300

    lesson_4 added

---
+++ lesson_4/dedup.py
@@ -0,0 +1,24 @@
+import sys
+from pympler.asizeof import asizeof
+users: list[str] = ["john", "marry", "mark", "jack", "marry", "mark"]
+
+# users_seen = set()
+# for user in users:
+#     if user in users_seen:
+#         continue
+#     users_seen.add(user)
+#     print(user) 
+
+def dedup(collection):
+    items = set()
+    for item in collection:
+        if item in items:
+            continue
+        items.add(item)
+        yield item
+
+
+for user in dedup(users):
+    print(user)
+
+print(asizeof(users))
