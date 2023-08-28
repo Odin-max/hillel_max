5ca4a104947e93f1da88ba213c6fa2b03bf8d427
meh84 <meh84741@gmail.com>
2023-08-28 21:24:05 +0300

    lesson 6

---
+++ lesson_6/basics.py
@@ -0,0 +1,23 @@
+from typing import Callable, Any
+
+
+def logger(func:Callable) -> Callable:
+    print(f"Running the {func.__name__}...")
+    
+    def inner(name:str):
+        results: Any = func(name)
+        if results:
+            print(f'Results {results}')
+        else:
+            print("There is no results...")
+
+
+    return inner
+
+
+@logger
+def greeting(name:str) -> None :
+    print(f"Hey {name}!")
+    
+greeting("john")
+# logger(greeting)(name="Max")
