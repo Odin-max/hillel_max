f6caad239c9b23a3e72a8c1c95b7de25c09abbb8
meh84 <meh84741@gmail.com>
2023-08-28 21:24:05 +0300

    lesson 6

---
+++ lesson_6/builtin.py
@@ -0,0 +1,53 @@
+# class Person:
+#     def __init__(self, name:str, age:int) -> None:
+#         self.name:str = name
+#         self.age: int = age
+import time
+class Person:
+    _instance = None
+    _initialized: bool = False
+
+    def perf_counter(func):
+        def inner(*args,**kwargs):
+            start = time.perf_counter()
+            result = func(*args,**kwargs)
+            exec_time = time.perf_counter()- start
+            print(f"Exec time: {func.__name__}:{exec_time}")
+            return result
+        return inner
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super().__new__(cls)
+        
+        return cls._instance
+    
+    def __init__(self,name:str,age:int) -> None:
+        if self._initialized:
+            return
+        self.name:str = name
+        self.age:int = age
+        self.is_adult:bool = False   
+        self._initialized = True
+
+    # @classmethod
+    @staticmethod
+    @perf_counter
+    def greeting(name:str):
+        print(f"Hello from {name}")
+
+    @property
+    def is_adultt(self)-> bool:
+        if self.age < 18:
+            return False
+        return True
+    
+john = Person(name="John", age=12)
+
+marry = Person(name="Marry", age=12)
+
+Person.greeting(john.name)
+john.greeting(name=john.name)
+
+print(john.is_adultt)
+# print(john.name)
+# print(marry.name)
