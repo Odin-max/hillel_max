250239b22567e8e15ebf5e2e82c8ad30a765788d
meh84 <meh84741@gmail.com>
2023-08-28 22:04:03 +0300

    fix all

--- lesson_6/printer.py
+++ lesson_6/printer.py
@@ -1,24 +1,29 @@
 class Printer:
-    def __init__(self,name:str, ip:str, port:int ) -> None:
+    def __init__(self, name: str, ip: str, port: int) -> None:
         self.name = name
         self.ip = ip
         self.port = port
+
     def __str__(self) -> str:
         return f"{self.name}@{self.ip}:{self.port}"
-    
-def open_connection(printer: Printer):
-    print(f'Connecton with Driver...')
-    print(f'Connecton with {printer}...')
 
-def print_with_printer(printer:Printer,text:str):
-    print(f'Printing with {printer}')
+
+def open_connection(printer: Printer):
+    print("Connecton with Driver...")
+    print(f"Connecton with {printer}...")
+
+
+def print_with_printer(printer: Printer, text: str):
+    print(f"Printing with {printer}")
     print(f"'{text}'")
 
-def close_connection(printer:Printer):
-    print(f'Closing connection for {printer}')
+
+def close_connection(printer: Printer):
+    print(f"Closing connection for {printer}")
+
 
 def connect(func):
-    def inner(printer: Printer, document:str):
+    def inner(printer: Printer, document: str):
         open_connection(printer)
         func(printer, document)
         close_connection(printer)
@@ -27,15 +32,16 @@ def connect(func):
 
 
 @connect
-def print_document(printer:Printer, document:str):
-    print(f"Document")
-
-hp_black = Printer(name='HP black', ip = "10.10.10.10.2", port = 33112)
-hp_white = Printer(name='HP black', ip = "10.10.10.10.15", port = 33112)
-hp_yellow = Printer(name='HP black', ip = "10.10.10.10.32", port = 33111)
+def print_document(printer: Printer, document: str):
+    print("Document")
 
 
-print_document(hp_black, '')
+hp_black = Printer(name="HP black", ip="10.10.10.10.2", port=33112)
+hp_white = Printer(name="HP black", ip="10.10.10.10.15", port=33112)
+hp_yellow = Printer(name="HP black", ip="10.10.10.10.32", port=33111)
+
+
+print_document(hp_black, "")
 
 
 # open_connection(hp_black)
--- lesson_6/smth.py
+++ lesson_6/smth.py
@@ -1,44 +1,51 @@
-from  functools import wraps
+from functools import wraps
+
 
 class Printer:
-    def __init__(self,name:str, ip:str, port:int ) -> None:
+    def __init__(self, name: str, ip: str, port: int) -> None:
         self.name = name
         self.ip = ip
         self.port = port
+
     def __str__(self) -> str:
         return f"{self.name}@{self.ip}:{self.port}"
-    
-    
-def open_connection(printer: Printer):
-    print(f'Connecton with Driver...')
-    print(f'Connecton with {printer}...')
 
-def print_with_printer(printer:Printer,text:str):
-    print(f'Printing with {printer}')
+
+def open_connection(printer: Printer):
+    print(f"Connecton with Driver...")
+    print(f"Connecton with {printer}...")
+
+
+def print_with_printer(printer: Printer, text: str):
+    print(f"Printing with {printer}")
     print(f"'{text}'")
 
-def close_connection(printer:Printer):
-    print(f'Closing connection for {printer}')
+
+def close_connection(printer: Printer):
+    print(f"Closing connection for {printer}")
+
 
 def connect(printer: Printer):
     def wrapper(func):
         @wraps(func)
-        def inner (document):
+        def inner(document):
             open_connection(printer)
             func(document)
             close_connection(printer)
+
         return inner
-    
+
     return wrapper
 
 
-hp_black = Printer(name='HP black', ip = "10.10.10.10.2", port = 33112)
-hp_white = Printer(name='HP black', ip = "10.10.10.10.15", port = 33112)
-hp_yellow = Printer(name='HP black', ip = "10.10.10.10.32", port = 33111)
+hp_black = Printer(name="HP black", ip="10.10.10.10.2", port=33112)
+hp_white = Printer(name="HP black", ip="10.10.10.10.15", port=33112)
+hp_yellow = Printer(name="HP black", ip="10.10.10.10.32", port=33111)
 
 
 @connect(printer=hp_black)
-def print_document(document:str):
+def print_document(document: str):
     print(f"document: {document}")
+
 
 print_document("John is on printer")
